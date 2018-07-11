ec16da3b36f67c92729623d03a5af18f15f60535
App Academy Student <appacademy@Apps-Mac-mini-2.local>
2018-07-10 17:15:15 -0700

    Pull form out into a partial

--- ninetyninecats/app/controllers/cats_controller.rb
+++ ninetyninecats/app/controllers/cats_controller.rb
@@ -37,8 +37,15 @@ class CatsController < ApplicationController
     @cat = Cat.find(params[:id])
     render :edit 
     
-    
   end 
   
+  def update 
+    @cat = Cat.find(params[:id])
+    if @cat.update(cat_params)
+      render :show 
+    else 
+      render json: cat.errors.full_messages
+    end 
+  end
     
 end 
